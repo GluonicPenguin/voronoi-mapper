b8f823dad8d1332a07eba35fffbe7aea9155c2a6
GluonicPenguin <gp19133@bristol.ac.uk>
2021-07-08 14:29:38 +0200

    Added setup script

---
+++ setup_env.sh
@@ -0,0 +1,17 @@
+export MINICONDA_DIR=../miniconda3
+source $MINICONDA_DIR/etc/profile.d/conda.sh
+conda activate voronoi_mapper_env
+
+echo '                                                                '
+echo '                                                                '
+echo '                                                             /$$'
+echo '                                                            |__/'
+echo ' /$$    /$$ /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$  /$$'
+echo '|  $$  /$$//$$__  $$ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$| $$'
+echo ' \  $$/$$/| $$  \ $$| $$  \__/| $$  \ $$| $$  \ $$| $$  \ $$| $$'
+echo '  \  $$$/ | $$  | $$| $$      | $$  | $$| $$  | $$| $$  | $$| $$'
+echo '   \  $/  |  $$$$$$/| $$      |  $$$$$$/| $$  | $$|  $$$$$$/| $$'
+echo '    \_/    \______/ |__/       \______/ |__/  |__/ \______/ |__/'
+echo '                                                                '
+echo '                                                                '
+
